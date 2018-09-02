4dac90c256086588def76fe11e430ccae2de0995
Arturo <alopez96@ucsc.edu>
2018-09-02 07:53:03 -0700

    init

---
+++ src/Hello.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello world2</h1>
+          <p>welcome to react</p>
+        </header>
+        <p className="f1 tc">test</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Hello from './Hello';
+import 'tachyons';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Hello/> , document.getElementById('root'));
+registerServiceWorker();
